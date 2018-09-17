0c4509d6136566f5fe1d1c0abcbca373945b156c
Saket <sk846694@gmail.com>
2018-09-18 00:55:52 +0530

    scan error handled

--- bot.c
+++ bot.c
@@ -2,7 +2,7 @@
 
 int main()
 {
-	int orient,arr[13][13]={0},bomb,i.,j;	//orient=Orientation,bomb=bomb move left
+	int orient,arr[13][13]={0},bomb,i,j;	//orient=Orientation,bomb=bomb move left
 	scanf("%d",&orient);
 	for(i=0;i<13;i++)
 		for(j=0;j<13;j++)
