f91fbe85bc717c671eb2614787f981ccc851af7a
Saket <sk846694@gmail.com>
2018-09-18 00:47:03 +0530

    scanning

--- bot.c
+++ bot.c
@@ -1,7 +1,12 @@
 #include<stdio.h>
+
 int main()
 {
-	printf("Hello World!");
-	printf("pushing to remote");
+	int orient,arr[13][13]={0},bomb,i.,j;	//orient=Orientation,bomb=bomb move left
+	scanf("%d",&orient);
+	for(i=0;i<13;i++)
+		for(j=0;j<13;j++)
+			scanf("%d",&arr[i][j]);
+	scanf("%d",&bomb);
 	return 0;
 }
