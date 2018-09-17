add738539c77fb27ee3967519d1ad360a2f12081
Saket <sk846694@gmail.com>
2018-09-18 00:46:08 +0530

    scanning

--- bot.c
+++ bot.c
@@ -1,6 +1,12 @@
 #include<stdio.h>
+
 int main()
 {
-	printf("Hello World!");
+	int orient,arr[13][13]={0},bomb,i.,j;	//orient=Orientation,bomb=bomb move left
+	scanf("%d",&orient);
+	for(i=0;i<13;i++)
+		for(j=0;j<13;j++)
+			scanf("%d",&arr[i][j]);
+	scanf("%d",&bomb);
 	return 0;
 }
